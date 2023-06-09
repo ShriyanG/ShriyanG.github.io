74d716d33757a76744654448526344ee7e47f3b7
ShriyanG <68485981+ShriyanG@users.noreply.github.com>
2023-06-08 20:49:37 -0500

    develop.original content

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to My Website, my page is being built</h1>
+    </header>
+  );
+};
+
+export default Header;
