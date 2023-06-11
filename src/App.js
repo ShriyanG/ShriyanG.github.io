a1e12f5d702419a22fd991936609dd9644c2bfdc
ShriyanG <68485981+ShriyanG@users.noreply.github.com>
2023-06-11 05:42:34 -0500

    develop. add background and navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ParticleComponent from "./components/ParticlesBackground";
+import Navbar from "./components/Navbar";
+import TestSection from "./components/TestSection";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <Navbar />
       <ParticleComponent />
+      <div style={{ position: "absolute", top: "100%", width: "100%" }}>
+        <TestSection />
+      </div>
     </div>
   );
 };
-
 export default App;
---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,101 @@
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import backgroundImage from "./background.png";
+import "./ParticleComponent.css"; // Import a separate CSS file for styles
+
+const ParticleComponent = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div className="particle-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+            background: {
+                image: `url(${backgroundImage})`, // Use the imported image
+                position: "50% 50%", // Adjust the position of the background image
+                repeat: "no-repeat", // Set the repeat behavior of the background image
+                size: "cover", // Adjust the size of the background image
+            },
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                    resize: true,
+                },
+                modes: {
+                    push: {
+                        quantity: 4,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: "#ffffff",
+                },
+                links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                collisions: {
+                    enable: true,
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                        default: "bounce",
+                    },
+                    random: false,
+                    speed: 2,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                        area: 800,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: 0.5,
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 1, max: 5 },
+                },
+            },
+            detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default ParticleComponent;
