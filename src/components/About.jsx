65b32b737a0a64245db5812314987b9756a1f2cd
ShriyanG <shriyan.gosavi@gmail.com>
2023-06-23 01:00:50 -0500

    Fix shaping issue.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import profileImage from "./pictures/profile.jpg"
+import profileImage from "./pictures/profile.jpg";
+
 const AboutSection = () => {
   return (
-    <div className = "about">
-      <img className="profile-image" src={profileImage} alt="Profile" />
+    <div className="about">
+      <div className="profile-container">
+        <img className="profile-image" src={profileImage} alt="Profile" />
+        <div className="text-container">
+          <p className="hello-text">Hello!</p>
+        </div>
+      </div>
     </div>
   );
 };
