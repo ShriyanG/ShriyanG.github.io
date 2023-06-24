4a1e66b376380e320e140a0db78505bb026ab76a
ShriyanG <shriyan.gosavi@gmail.com>
2023-06-24 15:07:52 -0500

    Finish about section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,10 @@ const AboutSection = () => {
     <div className="about">
       <div className="profile-container">
         <img className="profile-image" src={profileImage} alt="Profile" />
-        <div className="text-container">
-          <p className="hello-text">Hello!</p>
-        </div>
+        <p className="hello-text">Hello!</p>
+      </div>
+      <div className="additional-info">
+        <p>My name is Shriyan and I am a computer science student with a passion for machine learning, data analytics, and software engineering. I am a continuous learner who is eager for opportunities to employ my diverse skill set to solve real-world problems.</p>
       </div>
     </div>
   );
